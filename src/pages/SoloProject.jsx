1d831ec583d586c9aa12daf443afd9183a023314
Anna Maria Lester <anna47c4@stud.kea.dk>
2023-03-23 15:36:26 +0100

    case logo working on single case

--- src/pages/SoloProject.jsx
+++ src/pages/SoloProject.jsx
@@ -12,8 +12,22 @@ function SoloProject({ caseData }) {
 
   return (
     <>
-      <h1>{theCase.title}</h1>
-      <p>{theCase.longdesc}</p>
+      <section className="soloSection">
+        <div className="top-container">
+          <div className="logo">
+            {!theCase.logo && <p>no logo.</p>}{" "}
+            {theCase.logo && (
+              <img
+                src={theCase.logo}
+                alt={theCase.company}
+                className={theCase.company}
+              ></img>
+            )}
+          </div>
+        </div>
+        <h1>{theCase.title}</h1>
+        <p>{theCase.longdesc}</p>
+      </section>
     </>
   );
 }
